cb601f14cc60e2c6ee1eb98ee6f5006d25bc605c
Ilya Remenchik <ilyaremenchik228@gmail.com>
2025-05-26 17:42:37 +0300

    add booking, comments and db

---
+++ src/main/java/ru/practicum/shareit/booking/BookingMapper.java
@@ -0,0 +1,31 @@
+package ru.practicum.shareit.booking;
+
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.item.Item;
+import ru.practicum.shareit.item.ItemMapper;
+import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.user.UserMapper;
+
+public class BookingMapper {
+    public static BookingDto mapToBookingDto(Booking booking) {
+        return new BookingDto(
+                booking.getId(),
+                booking.getStart(),
+                booking.getEnd(),
+                ItemMapper.mapToItemShortDto(booking.getItem()),
+                UserMapper.mapToUserDto(booking.getBooker()),
+                booking.getStatus()
+        );
+    }
+
+    public static Booking mapToBooking(BookingDto bookingDto, Item item, User booker) {
+        Booking booking = new Booking();
+        booking.setId(bookingDto.getId());
+        booking.setStart(bookingDto.getStart());
+        booking.setEnd(bookingDto.getEnd());
+        booking.setStatus(bookingDto.getStatus());
+        booking.setBooker(booker);
+        booking.setItem(item);
+        return booking;
+    }
+}
--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,16 +1,22 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.booking.util.BookingStatus;
+import ru.practicum.shareit.item.dto.ItemShortDto;
+import ru.practicum.shareit.user.dto.UserDto;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookingDto {
-    private long id;
+    private Long id;
     private LocalDateTime start;
     private LocalDateTime end;
-    private long itemId;
-    private long bookerId;
+    private ItemShortDto item;
+    private UserDto booker;
     private BookingStatus status;
 }
